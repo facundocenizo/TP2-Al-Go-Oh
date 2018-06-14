d4020e61638e0ee9014bbe445f6e2adf14a9e425
BraianV <braian.e.a.villalba@gmail.com>
2018-06-14 06:06:23 -0300

    Modificaciones sobre las pruebas de la carta magica

--- src/fiuba/algo3/tp2/Cementerio.java
+++ src/fiuba/algo3/tp2/Cementerio.java
@@ -1,6 +1,8 @@
 package fiuba.algo3.tp2;
 
 import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.LinkedList;
 
 public class Cementerio {
 	
@@ -17,6 +19,23 @@ public class Cementerio {
 
 	public boolean contieneCarta(Carta carta) {
 		return cartasMuertas.contains(carta);
+	}
+
+	public void limpiarZonaDeMonstruosMuertos(LinkedList<Monstruo> zonaDeMonstruosDeJugador) {
+		//for (Monstruo unMonstruo: zonaDeMonstruosDeJugador) {
+		//	if (unMonstruo.estaMuerta()) {
+		//		cartasMuertas.add(unMonstruo);
+		//		//zonaDeMonstruosDeJugador.remove(unMonstruo);
+		//	}
+		//}
+		for(Iterator<Monstruo> iter = zonaDeMonstruosDeJugador.iterator(); iter.hasNext();) {
+		    Monstruo unMonstruo = iter.next();
+		    if (unMonstruo.estaMuerta()) {
+		    	cartasMuertas.add(unMonstruo);
+		        iter.remove();
+		    }
+		}
+		
 	}
 
 
--- test/fiuba/algo3/tp2/PrimeraEntregaTest.java
+++ test/fiuba/algo3/tp2/PrimeraEntregaTest.java
@@ -131,6 +131,10 @@ public class PrimeraEntregaTest {
 		Efecto efectoAgujeroNegro = new EfectoAgujeroNegro(campo);
 		Magica cartaAgujeroNegro = new Magica(new BocaArriba(),efectoAgujeroNegro);
 		cartaAgujeroNegro.aplicarEfecto();
+		campo.barrerZonaDeMonstruos();
+		
+		assertEquals(0,campo.cantidadDeMonstruosActivos());
+		
 		
 	}
 

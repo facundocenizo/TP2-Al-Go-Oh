a24f55c3471034add03619dbafbad43c9827e59f
Pablo <Pablo@DESKTOP-B3VOE8A>
2018-06-21 03:17:38 -0300

    mas pruebas segunda entrega

--- test/fiuba/algo3/tp2/segundaEntrega/cartaDragonDefinitivoTest.java
+++ test/fiuba/algo3/tp2/segundaEntrega/cartaDragonDefinitivoTest.java
@@ -1,5 +1,11 @@
 package fiuba.algo3.tp2.segundaEntrega;
 
+import org.junit.Test;
+
 public class cartaDragonDefinitivoTest {
 
+	@Test
+	public void activarFisuraDestruyeElMonstruoConMenorPuntosDeAtaqueDelOponente() {
+	
+	}
 }
